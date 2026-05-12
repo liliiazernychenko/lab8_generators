f832faabdac2d0f4704e711cf6903d7f0176fb8f
liliiazernychenko <lilavaon@gmail.com>
2026-05-12 11:12:48 +0300

    Update 8_3_2_test_d.py

--- 8_3_2_test_d.py
+++ 8_3_2_test_d.py
@@ -1,5 +1,6 @@
-def recursive_term(a_k_minus_2, k):
-    return (k - 1) / k * a_k_minus_2
+def recursive_term(a_k_minus_1, a_k_minus_2, k):
+    return a_k_minus_1 / k + a_k_minus_2
+
 
 def sequence_generator(n):
     a1 = 1
@@ -10,21 +11,31 @@ def sequence_generator(n):
         yield a2
 
     a_k_minus_2 = a1
-    a_k_minus_1 = a2 if n >= 2 else 0
+    a_k_minus_1 = a2
 
     for k in range(3, n + 1):
-        a_k = recursive_term(a_k_minus_2, k)
+        a_k = recursive_term(a_k_minus_1, a_k_minus_2, k)
+
         yield a_k
-        a_k_minus_2, a_k_minus_1 = a_k_minus_1, a_k
+
+        a_k_minus_2 = a_k_minus_1
+        a_k_minus_1 = a_k
+
 
 def compute_sum(n):
     total = 0
+    k = 1
+
     for value in sequence_generator(n):
-        total += value
+        total += (3 ** k) / value
+        k += 1
+
     return total
+
 
 def main():
     n = int(input("Enter n: "))
+
     print("Sn =", compute_sum(n))
 
 
